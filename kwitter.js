e0a114ee9058a8fdb20515fe747849d4e2e9f48c
Sahanastar <81565078+Sahanastar@users.noreply.github.com>
2021-10-13 08:31:26 +0530

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,3 +1,6 @@
 function adduser(){
     user_name = document.getElementById("username").value;
+    localStorage.setItem("User_Name",user_name);
+    window.location = "kwitter_room.html";
+    
 }
